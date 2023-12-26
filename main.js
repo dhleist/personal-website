0cd04de947bbea7e2ca5079e471acdf94236f8ec
dhleist <dhleist@gmail.com>
2023-12-26 11:08:47 -0600

    Change heading animation to play once per page session

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 const heading = document.querySelector("h1");
+heading.style.visibilty = "hidden";
+
 const finalTitle = heading.textContent;
 
-let currentTitle = finalTitle.charAt(0); 
+if (!sessionStorage.getItem("finishedAnimation")) { 
+  let currentTitle = finalTitle.charAt(0); 
 
-heading.textContent = currentTitle;
-heading.style.width = "1ch";
-heading.style.visibility = "visible";
+  heading.textContent = currentTitle;
+  heading.style.width = "1ch";
+  heading.style.visibility = "visible";
 
-let index = 1;
+  let index = 1;
 
-let intervalID = setInterval(() => {
-  if (index < finalTitle.length) {
-    let currentCharacter = finalTitle.charAt(index);
-    currentTitle = currentTitle.concat(currentCharacter); 
-    heading.textContent = currentTitle;
-    heading.style.width = `${index + 1}ch`;
-    index++;
-  }
-  else {
-    clearInterval(intervalID);
-  }
-}, 250);
+  let intervalID = setInterval(() => {
+    if (index < finalTitle.length) {
+      let currentCharacter = finalTitle.charAt(index);
+      currentTitle = currentTitle.concat(currentCharacter); 
+      heading.textContent = currentTitle;
+      heading.style.width = `${index + 1}ch`;
+      index++;
+    }
+    else {
+      clearInterval(intervalID);
+    }
+  }, 250);
+  
+  sessionStorage.setItem("finishedAnimation", "true");
+}
 
 const caret = document.querySelector("#caret");
 
 setInterval(() => {
-  if (currentTitle === finalTitle) { 
+  if (heading.textContent === finalTitle) { 
     caret.classList.toggle("hide");
   }
 }, 450);
